5d5a13a5e2d38ddf7a6d0cebc0af2212bc7af7f1
PainDeMie22 <84603374+PainDeMie22@users.noreply.github.com>
2021-05-21 23:31:33 +0100

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#keys.py
+#visit https://dev.twitter.com to create an application and get your keys
+keys = dict(
+    consumer_key =          'TohxTUWACLmic4BY33zske7tn',
+    consumer_secret =       'DYutQYTRPsEoH3pVEpHeWUAO4jkRcdszxsAfMNw8puzxJu3WWz',
+    access_token =          '1050478388727693312-E3JFAJK8HYFdYrtqfnQRIKcxYIjKCv',
+    access_token_secret =   'WBK9yV4vOWKGH8zie2oYmhhtaoo91JIEENsMewfE6bbrT',
+)
